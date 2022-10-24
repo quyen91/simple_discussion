563d98defc253903b2e9bbcc3288f76903710099
quyen van <kuto@kutoda.local>
2022-10-24 16:32:51 +0700

    paginate - fix typo

--- lib/simple_discussion.rb
+++ lib/simple_discussion.rb
@@ -7,7 +7,7 @@ require "simple_discussion/forum_user"
 require "simple_discussion/slack"
 require "simple_discussion/version"
 # require "simple_discussion/will_paginate"
-require "kaminari/helpers/tag"
+require "lib/kaminari/helpers/tag"
 
 module SimpleDiscussion
   # Define who owns the subscription
